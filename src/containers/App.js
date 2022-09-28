3da7f5e332a4e641fe4586ae3fbe021f31329a47
Rafhy <you>
2022-09-28 08:47:38 -0600

    Uploading Copy of Robofriends

---
+++ src/containers/App.js
@@ -0,0 +1,53 @@
+import React, {Component} from 'react';
+import CardList from '../components/CardList';
+import SearchBox from '../components//SearchBox';
+import Scroll from '../components/Scroll';
+import './App.css';
+import ErrorBoundry from '../components/ErrorBoundry';
+
+
+class App extends Component {
+	constructor () {
+		super()
+		this.state = {
+			robots: [],
+			searchfield: ''	
+		}
+	}
+
+
+	componentDidMount (){
+		fetch('https://jsonplaceholder.typicode.com/users')
+		.then(response=> response.json())
+		.then(users => this.setState({ robots:users}));
+	}
+
+	onSearchChange = (event) => {
+		this.setState({ searchfield: event.target.value})
+	}
+
+
+	render () {
+	  const { robots, searchfield } = this.state;
+	  const  filteredRobots = robots.filter(robot => {
+		return robot.name.toLowerCase().includes(searchfield.toLowerCase());
+	  })
+	{/*creating a loading screen------since getting users from online----1sec in begining----*/}
+	{/* 0 means false, so opposite for true*/}
+	  return !robots.length ?
+	  	<h1>Loading</h1> :
+	  	  (
+			<div className ='tc'>
+			  <h1 className='f1'>RoboFriends</h1>
+			  <SearchBox searchChange={this.onSearchChange}/>
+			  <Scroll>
+			  	<ErrorBoundry>
+			  		<CardList robots={filteredRobots}/>
+			  	</ErrorBoundry>
+			  </Scroll>
+			</div>
+		  );
+	}
+}
+
+export default App;
